385e6bc2cde48919dce0630b741bdf1a26d31289
Stainley Lebron <saiyamans@gmail.com>
2023-03-01 18:41:43 -0500

    HAB-58 design and removed one emulator

--- app/src/main/java/ca/lambton/habittracker/view/fragment/progress/DailyProgressFragment.java
+++ app/src/main/java/ca/lambton/habittracker/view/fragment/progress/DailyProgressFragment.java
@@ -1,2 +1,26 @@
-package ca.lambton.habittracker.view.fragment.progress;public class DailyProgressFragment {
+package ca.lambton.habittracker.view.fragment.progress;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import ca.lambton.habittracker.databinding.FragmentDailyHabitProgressBinding;
+
+public class DailyProgressFragment extends Fragment {
+
+    private FragmentDailyHabitProgressBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentDailyHabitProgressBinding.inflate(inflater);
+
+
+        return binding.getRoot();
+    }
 }
