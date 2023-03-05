2109d0a6d7215ccf03988207468e946b201dcc1d
Stainley Lebron <saiyamans@gmail.com>
2023-03-04 22:34:55 -0500

    card view

--- app/src/main/java/ca/lambton/habittracker/view/settings/SettingsFragment.java
+++ app/src/main/java/ca/lambton/habittracker/view/settings/SettingsFragment.java
@@ -3,64 +3,31 @@ package ca.lambton.habittracker.view.settings;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import ca.lambton.habittracker.R;
+import ca.lambton.habittracker.databinding.FragmentSettingsBinding;
+import ca.lambton.habittracker.view.fragment.habit.CollectionCardCategoryFragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SettingsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    FragmentSettingsBinding binding;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        binding = FragmentSettingsBinding.inflate(inflater);
+        View view = binding.getRoot();
+
+        Fragment collectionCardCategoryFragment = new CollectionCardCategoryFragment();
+        FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction();
+
+        fragmentTransaction.replace(R.id.stack_view, collectionCardCategoryFragment).commit();
+
+        return view;
     }
 }
