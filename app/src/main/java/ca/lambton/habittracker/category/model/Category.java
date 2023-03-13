47d8099cff585ff104d9dd15b0cf722fa214384a
Ann Robles <annr.dev@gmail.com>
2023-03-12 20:22:36 -0400

    HAB-45  Add pre-define categories in the database

--- app/src/main/java/ca/lambton/habittracker/category/model/Category.java
+++ app/src/main/java/ca/lambton/habittracker/category/model/Category.java
@@ -8,6 +8,8 @@ import androidx.room.PrimaryKey;
 import java.io.Serializable;
 import java.util.Date;
 
+import ca.lambton.habittracker.R;
+
 @Entity(tableName = "CATEGORY_TBL")
 public class Category implements Serializable {
     @PrimaryKey(autoGenerate = true)
@@ -112,5 +114,24 @@ public class Category implements Serializable {
 
     public void setUserId(int userId) {
         this.userId = userId;
+    }
+
+    public static Category[] populateData() {
+        return new Category[] {
+            new Category("Short Duration", "R.drawable.short_duration", 2, "5 - 10 mins", new Date(), new Date(), true, 0),
+            new Category("Long Duration", "R.drawable.long_duration", 3, "20 mins - 1 hour", new Date(), new Date(), true, 0),
+            new Category("Hobbies", "R.drawable.hobbies", 3, "30 mins", new Date(), new Date(), true, 0),
+            new Category("Outdoor Activities", "R.drawable.outdoor_activities", 3, "20 mins - 1 hour", new Date(), new Date(), true, 0),
+            new Category("Quit Bad Habits", "R.drawable.quit_bad_habits", 2, "20 mins - 1 hour", new Date(), new Date(), true, 0),
+            new Category("Food Habits", "R.drawable.foods_habit", 2, "20 mins - 1 hour", new Date(), new Date(), true, 0),
+            new Category("Socialize", "R.drawable.socialize", 2, "20 mins - 1 hour", new Date(), new Date(), true, 0),
+            new Category("Relaxation", "R.drawable.relaxation", 3, "20 mins - 1 hour", new Date(), new Date(), true, 0),
+            new Category("Physical Health", "R.drawable.physical_health", 3, "20 mins - 1 hour", new Date(), new Date(), true, 0),
+            new Category("Mental Health", "R.drawable.mental_health", 3, "20 mins - 1 hour", new Date(), new Date(), true, 0),
+            new Category("Daily", "R.drawable.daily", 0, "20 mins - 1 hour", new Date(), new Date(), true, 0),
+            new Category("Weekly", "R.drawable.weekly", 0, "20 mins - 1 hour", new Date(), new Date(), true, 0),
+            new Category("Monthly", "R.drawable.monthly", 0, "20 mins - 1 hour", new Date(), new Date(), true, 0),
+            new Category("Self Care", "R.drawable.self_care", 2, "10 - 30 mins", new Date(), new Date(), true, 0),
+        };
     }
 }
