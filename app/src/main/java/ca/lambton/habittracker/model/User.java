ca1a3e30a3e07526a484fcc70b0763ee7fc9e882
Stainley Lebron <saiyamans@gmail.com>
2023-03-12 21:16:17 -0400

    user entity

---
+++ app/src/main/java/ca/lambton/habittracker/model/User.java
@@ -0,0 +1,46 @@
+package ca.lambton.habittracker.model;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class User {
+
+    @PrimaryKey
+    private long id;
+    private String accountId;
+    private String name;
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
