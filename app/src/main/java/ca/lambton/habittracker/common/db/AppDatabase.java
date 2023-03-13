fc639de715a7158f3c53349483716c431ed8bcda
Ann Robles <annr.dev@gmail.com>
2023-03-12 20:23:03 -0400

    HAB-45  Add pre-define categories in the database (#20)

--- app/src/main/java/ca/lambton/habittracker/common/db/AppDatabase.java
+++ app/src/main/java/ca/lambton/habittracker/common/db/AppDatabase.java
@@ -1,11 +1,14 @@
 package ca.lambton.habittracker.common.db;
 
 import android.app.Application;
+import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -13,12 +16,14 @@ import java.util.concurrent.Executors;
 import ca.lambton.habittracker.category.dao.CategoryDao;
 import ca.lambton.habittracker.category.model.Category;
 import ca.lambton.habittracker.common.dao.PictureDao;
+import ca.lambton.habittracker.common.helper.Converters;
 import ca.lambton.habittracker.common.model.Picture;
 
 @Database(entities = {
         Category.class,
         Picture.class
 }, version = 1, exportSchema = false)
+@TypeConverters({Converters.class})
 public abstract class AppDatabase extends RoomDatabase {
 
     private static volatile AppDatabase INSTANCE;
@@ -26,10 +31,30 @@ public abstract class AppDatabase extends RoomDatabase {
     private static final int NUMBER_OF_THREADS = 4;
     public static final ExecutorService databaseWriterExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
-    public static AppDatabase getInstance(@NonNull final Application application) {
+    public synchronized static AppDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = getDatabase(context);
+        }
+        return INSTANCE;
+    }
+
+    public static AppDatabase getDatabase(Context context) {
         if (INSTANCE == null) {
             synchronized (AppDatabase.class) {
-                INSTANCE = Room.databaseBuilder(application, AppDatabase.class, "task-note-db").build();
+                INSTANCE = Room.databaseBuilder(context, AppDatabase.class, "habit_tracker_db")
+                        .addCallback(new Callback() {
+                            @Override
+                            public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                                super.onCreate(db);
+                                Executors.newSingleThreadScheduledExecutor().execute(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        getInstance(context).categoryDao().insertAll(Category.populateData());
+                                    }
+                                });
+                            }
+                        })
+                        .build();
             }
         }
         return INSTANCE;
