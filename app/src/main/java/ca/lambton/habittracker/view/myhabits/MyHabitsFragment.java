3a969ddd381f45a1d12f602c0451b843bf62bb3c
Ann Robles <annr.dev@gmail.com>
2023-03-19 00:43:29 -0400

    HAB-134 Create the ongoing habit main screen

--- app/src/main/java/ca/lambton/habittracker/view/myhabits/MyHabitsFragment.java
+++ app/src/main/java/ca/lambton/habittracker/view/myhabits/MyHabitsFragment.java
@@ -174,16 +174,14 @@ public class MyHabitsFragment extends Fragment {
 
     @NonNull
     private MyHabitsGridButtonAdapter.OnMyHabitsGridButtonCallback getCallbackMyHabitsGridButton(List<MyHabitsGridButton> myHabitsGridButton) {
-        return new MyHabitsGridButtonAdapter.OnMyHabitsGridButtonCallback() {
-
-            @Override
-            public void onRowClicked(int position) {
-                switch (position) {
-                    case 0:
-                        Navigation.findNavController(getView()).navigate(R.id.newHabitFragment);
-
-                        break;
-                }
+        return position -> {
+            switch (position) {
+                case 0:
+                    Navigation.findNavController(getView()).navigate(R.id.newHabitFragment);
+                    break;
+                case 2:
+                    Navigation.findNavController(getView()).navigate(R.id.ongoingHabitFragment);
+                    break;
             }
         };
     }
