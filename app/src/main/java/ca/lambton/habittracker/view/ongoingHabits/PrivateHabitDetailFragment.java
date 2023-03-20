b2f203a04b7fef2d7110734b02004e83e0ce5a55
Ann Robles <annr.dev@gmail.com>
2023-03-20 14:55:00 -0400

    HAB-99 Created the habit detail grid info progress

--- app/src/main/java/ca/lambton/habittracker/view/ongoingHabits/PrivateHabitDetailFragment.java
+++ app/src/main/java/ca/lambton/habittracker/view/ongoingHabits/PrivateHabitDetailFragment.java
@@ -16,15 +16,6 @@ public class PrivateHabitDetailFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PrivateHabitDetailFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static PrivateHabitDetailFragment newInstance(String param1, String param2) {
         PrivateHabitDetailFragment fragment = new PrivateHabitDetailFragment();
         Bundle args = new Bundle();
