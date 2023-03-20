e158177ad34ff934f8b98b169584010dea217151
Aswin_Sasikanth <sasikanthh@gmail.com>
2023-03-20 11:24:29 -0400

    Imlementation-declare views, objs for classes

---
+++ app/src/main/java/ca/lambton/habittracker/IntroPref.java
@@ -0,0 +1,11 @@
+package ca.lambton.habittracker;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class IntroPref {
+
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    Context context;
+}
--- app/src/main/java/ca/lambton/habittracker/OnBoarding.java
+++ app/src/main/java/ca/lambton/habittracker/OnBoarding.java
@@ -1,10 +1,23 @@
 package ca.lambton.habittracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.viewpager.widget.PagerAdapter;
+
 
 public class OnBoarding extends AppCompatActivity {
+
+    private TextView tvSkip;
+    private ViewPager viewPager;
+    private LinearLayout layoutDots;
+    private IntroPref introPref;
+    private int[] layouts;
+    private TextView[] dots;
+    private MyViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
