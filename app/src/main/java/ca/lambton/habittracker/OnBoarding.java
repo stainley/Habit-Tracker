99e13acc5427198640d46769232040152cc6c35d
Aswin_Sasikanth <sasikanthh@gmail.com>
2023-03-20 12:01:45 -0400

    status bar color function

--- app/src/main/java/ca/lambton/habittracker/OnBoarding.java
+++ app/src/main/java/ca/lambton/habittracker/OnBoarding.java
@@ -3,9 +3,12 @@ package ca.lambton.habittracker;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -24,38 +27,73 @@ public class OnBoarding extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_boarding);
-    }
-    introPref = new IntroPref(this);
+
+
+        introPref = new
+
+                IntroPref(this);
         if (!introPref.isFirstTimeLaunch()) {
-        launchHomeScreen();
-        finish();
-    }
-        if (Build.VERSION.SDK_INT >= 21) {
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                View.SYSTEM_UI_FLAG_FULLSCREEN);
-    }
-
-    tvSkip = findViewById(R.id.tvSkip);
-    viewPager = findViewById(R.id.viewPager);
-    layoutDots = findViewById(R.id.layoutDots);
-
-    layouts = new int[]{
-        R.layout.intro_one,
-                R.layout.intro_two,
-                R.layout.intro_three,
-                R.layout.intro_four
-    };
-
-    tvSkip.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            int current = getItem(+1);
-            if (current < layouts.length) {
-                // move to next screen
-                viewPager.setCurrentItem(current + 2);
-            } else {
-                launchHomeScreen();
-            }
+            launchHomeScreen();
+            finish();
         }
-    });
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                    View.SYSTEM_UI_FLAG_FULLSCREEN);
+        }
+
+        tvSkip =
+
+                findViewById(R.id.tvSkip);
+
+        viewPager =
+
+                findViewById(R.id.viewPager);
+
+        layoutDots =
+
+                findViewById(R.id.layoutDots);
+
+        layouts = new int[]
+
+                {
+                        R.layout.intro_one,
+                        R.layout.intro_two,
+                        R.layout.intro_three,
+                        R.layout.intro_four
+                }
+
+        ;
+
+        tvSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int current = getItem(+1);
+                if (current < layouts.length) {
+                    // move to next screen
+                    viewPager.setCurrentItem(current + 2);
+                } else {
+                    launchHomeScreen();
+                }
+            }
+        });
+
+        viewPagerAdapter = new
+
+                MyViewPagerAdapter();
+        viewPager.setAdapter(viewPagerAdapter);
+        viewPager.addOnPageChangeListener(onPageChangeListener);
+
+        addBottomDots(0);
+
+        changeStatusBarColor();
+    }
+
+    private void changeStatusBarColor() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
 }
