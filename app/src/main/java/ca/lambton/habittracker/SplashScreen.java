0bcb5b4ca2407248587bc8f2c3ee127ec7c0f63d
jayesh-khistria <115033392+jayesh-khistria@users.noreply.github.com>
2023-03-19 23:20:21 -0400

    rename the file to with proper naming convention

--- app/src/main/java/ca/lambton/habittracker/SplashScreen.java
+++ app/src/main/java/ca/lambton/habittracker/SplashScreen.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Splash_Screen#newInstance} factory method to
+ * Use the {@link SplashScreen#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Splash_Screen extends Fragment {
+public class SplashScreen extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class Splash_Screen extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Splash_Screen() {
+    public SplashScreen() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class Splash_Screen extends Fragment {
      * @return A new instance of fragment Splash_Screen.
      */
     // TODO: Rename and change types and number of parameters
-    public static Splash_Screen newInstance(String param1, String param2) {
-        Splash_Screen fragment = new Splash_Screen();
+    public static SplashScreen newInstance(String param1, String param2) {
+        SplashScreen fragment = new SplashScreen();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class Splash_Screen extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash__screen, container, false);
+        return inflater.inflate(R.layout.fragment_splashscreen, container, false);
     }
 }
