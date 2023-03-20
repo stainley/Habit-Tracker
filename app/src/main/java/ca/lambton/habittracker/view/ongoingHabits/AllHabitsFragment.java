6e89f27fd01a1bc353bc671d7de8bb8af188f61e
Ann Robles <annr.dev@gmail.com>
2023-03-19 23:18:36 -0400

    HAB-135 Create Screen for listing habit cards (#36)
    * HAB-135 Added the navigation on Ongoing habit screen to each ongoing private, group, public challenges
    
    * HAB-135 Hide the challenges for now
    
    * HAB-135 Create Screen for listing habit cards

---
+++ app/src/main/java/ca/lambton/habittracker/view/ongoingHabits/AllHabitsFragment.java
@@ -0,0 +1,85 @@
+package ca.lambton.habittracker.view.ongoingHabits;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.lambton.habittracker.databinding.FragmentAllHabitsBinding;
+import ca.lambton.habittracker.habit.model.Habit;
+import ca.lambton.habittracker.habit.viewmodel.HabitViewModel;
+import ca.lambton.habittracker.habit.viewmodel.HabitViewModelFactory;
+
+public class AllHabitsFragment extends Fragment {
+
+    FragmentAllHabitsBinding binding;
+
+    private OngoingHabitsRecycleAdapter privateOngoingHabitListAdapter;
+    private OngoingHabitsRecycleAdapter groupOngoingHabitListAdapter;
+
+    HabitViewModel habitViewModel;
+
+    private final List<Habit> habits = new ArrayList<>();
+
+    public AllHabitsFragment() {
+        // Required empty public constructor
+    }
+
+    public static AllHabitsFragment newInstance(String param1, String param2) {
+        AllHabitsFragment fragment = new AllHabitsFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentAllHabitsBinding.inflate(inflater, container, false);
+        RecyclerView recyclerView = binding.privateOngoingHabitsList;
+        RecyclerView groupRecyclerView = binding.groupOngoingHabitsList;
+
+        habitViewModel = new ViewModelProvider(this, new HabitViewModelFactory(getActivity().getApplication())).get(HabitViewModel.class);
+        habitViewModel.getAllHabit().observe(getViewLifecycleOwner(), result -> {
+            this.habits.clear();
+            this.habits.addAll(result);
+            privateOngoingHabitListAdapter.notifyDataSetChanged();
+            groupOngoingHabitListAdapter.notifyDataSetChanged();
+        });
+
+        privateOngoingHabitListAdapter = new OngoingHabitsRecycleAdapter(habits, getOnCallbackOngoingHabit(habits), this.getContext(), false);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        recyclerView.setAdapter(privateOngoingHabitListAdapter);
+
+        groupOngoingHabitListAdapter = new OngoingHabitsRecycleAdapter(habits, getOnCallbackOngoingHabit(habits), this.getContext(), true);
+        groupRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        groupRecyclerView.setAdapter(groupOngoingHabitListAdapter);
+
+        return binding.getRoot();
+    }
+
+    @NonNull
+    private OngoingHabitsRecycleAdapter.OnOngoingHabitsCallback getOnCallbackOngoingHabit(List<Habit> habits) {
+        return new OngoingHabitsRecycleAdapter.OnOngoingHabitsCallback() {
+
+            @Override
+            public void onRowClicked(int position) {
+
+            }
+        };
+    }
+}
--- app/src/main/java/ca/lambton/habittracker/view/ongoingHabits/OngoingHabitsFragment.java
+++ app/src/main/java/ca/lambton/habittracker/view/ongoingHabits/OngoingHabitsFragment.java
@@ -1,49 +1,27 @@
 package ca.lambton.habittracker.view.ongoingHabits;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import ca.lambton.habittracker.R;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OngoingHabitsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import ca.lambton.habittracker.R;
+import ca.lambton.habittracker.databinding.FragmentOngoingHabitsBinding;
+
 public class OngoingHabitsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FragmentOngoingHabitsBinding binding;
 
     public OngoingHabitsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OngoingHabitsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static OngoingHabitsFragment newInstance(String param1, String param2) {
         OngoingHabitsFragment fragment = new OngoingHabitsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,16 +29,35 @@ public class OngoingHabitsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ongoing_habits, container, false);
+        View view = inflater.inflate(R.layout.fragment_ongoing_habits, container, false);
+        binding = FragmentOngoingHabitsBinding.inflate(inflater, container, false);
+
+        binding.allHabitsCard.setOnClickListener(this::allHabitsCardClicked);
+        binding.privateHabitsCard.setOnClickListener(this::privateHabitsCardClicked);
+        binding.groupHabitsCard.setOnClickListener(this::groupHabitsCardClicked);
+        //binding.publicChallengesCard.setOnClickListener(this::publicChallengesCardClicked);
+
+        return binding.getRoot();
+    }
+
+    private void allHabitsCardClicked(View view) {
+        Navigation.findNavController(getView()).navigate(R.id.allHabitsFragment);
+    }
+
+    private void privateHabitsCardClicked(View view) {
+        Navigation.findNavController(getView()).navigate(R.id.privateHabitsFragment);
+    }
+
+    private void groupHabitsCardClicked(View view) {
+        Navigation.findNavController(getView()).navigate(R.id.groupHabitsFragment);
+    }
+
+    private void publicChallengesCardClicked(View view) {
+        Navigation.findNavController(getView()).navigate(R.id.publicChallengesFragment);
     }
 }
