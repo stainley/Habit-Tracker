ec8c47002ea344e1232e489ab35051322a974291
Stainley Lebron <saiyamans@gmail.com>
2023-03-23 14:37:57 -0400

    new graph library, progress bar with real value and progress

--- app/src/main/java/ca/lambton/habittracker/view/fragment/progress/TodayReportAdapter.java
+++ app/src/main/java/ca/lambton/habittracker/view/fragment/progress/TodayReportAdapter.java
@@ -9,11 +9,17 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.antgroup.antv.f2.F2CanvasView;
-import com.github.mikephil.charting.charts.HorizontalBarChart;
-import com.github.mikephil.charting.charts.LineChart;
+import com.antgroup.antv.f2.F2Chart;
 import com.google.android.material.progressindicator.CircularProgressIndicator;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import ca.lambton.habittracker.R;
 import ca.lambton.habittracker.habit.model.HabitProgress;
@@ -23,9 +29,12 @@ public class TodayReportAdapter extends RecyclerView.Adapter<TodayReportAdapter.
     private final List<HabitProgress> habitProgresses;
     private final OnTodayReportChangeListener onTodayReportChangeListener;
 
+
     public TodayReportAdapter(List<HabitProgress> habitProgresses, OnTodayReportChangeListener onTodayReportChangeListener) {
         this.habitProgresses = habitProgresses;
         this.onTodayReportChangeListener = onTodayReportChangeListener;
+
+
     }
 
     @NonNull
@@ -37,7 +46,12 @@ public class TodayReportAdapter extends RecyclerView.Adapter<TodayReportAdapter.
 
     @Override
     public void onBindViewHolder(@NonNull TodayReportViewHolder holder, int position) {
-        holder.progressIndicator.setProgress(25);
+
+        double totalCompleted = habitProgresses.get(position).getProgressList().stream().count();
+        double frequency = Double.parseDouble(habitProgresses.get(position).getHabit().getFrequency());
+        int percentageValue = (int) ((totalCompleted / frequency) * 100);
+        holder.percentage.setText(String.format(Locale.CANADA, "%d%%", percentageValue));
+        holder.progressIndicator.setProgress(percentageValue);
         holder.habitName.setText(habitProgresses.get(position).getHabit().getName());
 
         this.onTodayReportChangeListener.onGraphChange(holder.canvasView, position);
@@ -49,13 +63,14 @@ public class TodayReportAdapter extends RecyclerView.Adapter<TodayReportAdapter.
         return habitProgresses.size();
     }
 
+
     static class TodayReportViewHolder extends RecyclerView.ViewHolder {
 
         private final CircularProgressIndicator progressIndicator;
         private final TextView habitName;
         //private final LineChart graphChart;
-
         private final F2CanvasView canvasView;
+        private final TextView percentage;
 
         public TodayReportViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -63,6 +78,7 @@ public class TodayReportAdapter extends RecyclerView.Adapter<TodayReportAdapter.
             habitName = itemView.findViewById(R.id.habit_text);
             //graphChart = itemView.findViewById(R.id.graph_chart);
             canvasView = itemView.findViewById(R.id.canvas_view);
+            percentage = itemView.findViewById(R.id.percent_num);
         }
     }
 
--- app/src/main/java/ca/lambton/habittracker/view/fragment/progress/TodayReportFragment.java
+++ app/src/main/java/ca/lambton/habittracker/view/fragment/progress/TodayReportFragment.java
@@ -1,57 +1,44 @@
 package ca.lambton.habittracker.view.fragment.progress;
 
-import android.graphics.Color;
-import android.graphics.DashPathEffect;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.provider.CalendarContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.antgroup.antv.f2.F2CanvasView;
 import com.antgroup.antv.f2.F2Chart;
 import com.github.mikephil.charting.charts.HorizontalBarChart;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.formatter.IFillFormatter;
-import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
-import ca.lambton.habittracker.R;
 import ca.lambton.habittracker.databinding.FragmentRecycleViewBinding;
-import ca.lambton.habittracker.habit.model.Habit;
 import ca.lambton.habittracker.habit.model.HabitProgress;
-import ca.lambton.habittracker.habit.model.Progress;
-import ca.lambton.habittracker.util.Utils;
+import ca.lambton.habittracker.habit.viewmodel.HabitViewModel;
+import ca.lambton.habittracker.habit.viewmodel.HabitViewModelFactory;
 
 public class TodayReportFragment extends Fragment implements F2CanvasView.Adapter {
 
     private FragmentRecycleViewBinding binding;
     private TodayReportAdapter todayReportAdapter;
+    private final List<HabitProgress> habitProgressList = new ArrayList<>();
+    private HabitViewModel habitViewModel;
 
-    private LineChart chart;
-
-    private F2Chart mChart;
-    private F2CanvasView canvasView;
-
+    //private F2Chart mChart;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -59,40 +46,91 @@ public class TodayReportFragment extends Fragment implements F2CanvasView.Adapte
 
         binding = FragmentRecycleViewBinding.inflate(LayoutInflater.from(requireContext()));
         RecyclerView recycleView = binding.recycleView;
-
-        List<HabitProgress> habitProgressList = new ArrayList<>();
-        Habit habit = new Habit();
-        habit.setId(1);
-        habit.setName("Running");
-
-        List<Progress> progressList = new ArrayList<>();
-        Progress progress1 = new Progress();
-        progress1.setCounter(1);
-        progress1.setUpdatedDate(new Date().getTime());
-        progress1.setHabitId(1);
-        progressList.add(progress1);
-
-        Progress progress2 = new Progress();
-        progress2.setCounter(1);
-        progress2.setUpdatedDate(new Date().getTime());
-        progress2.setHabitId(1);
-        progressList.add(progress2);
-
-
-        habitProgressList.add(new HabitProgress(habit, progressList));
-
-
+        habitViewModel = new ViewModelProvider(requireActivity(), new HabitViewModelFactory(requireActivity().getApplication())).get(HabitViewModel.class);
+       /* if (mChart == null) {
+            mChart = F2Chart.create(requireContext(), "LineChart-Kotlin", 700, 350);
+        }*/
         todayReportAdapter = new TodayReportAdapter(habitProgressList, (canvasView, position) -> {
+
+
             /*chart = lineChart;
             LineData lineData = setData(7, 12.0f);
             lineChart.setData(lineData);
             lineChart.notifyDataSetChanged();*/
-            this.canvasView = canvasView;
-            canvasView.initCanvasContext();
-            canvasView.setAdapter(this);
+            canvasView.setAdapter(new F2CanvasView.Adapter() {
+                F2Chart mChart;
+
+                @Override
+                public void onCanvasDraw(F2CanvasView canvasView) {
+
+                    //Initialize the chart, because you need the width and height of the canvasView,
+                    //initialize here to ensure that you can get the width and height of the canvasView
+
+                    if (mChart == null) {
+                        mChart = F2Chart.create(canvasView.getContext(), "Report", canvasView.getWidth(), canvasView.getHeight());
+                    }
+                    // 700 x 350
+                    //Associating the chart and canvasView, the chart finally needs to display the chart on the canvasView
+                    mChart.setCanvas(canvasView);
+                    //set chart padding
+                    mChart.padding(10.0, 10.0, 10.0, 10.0);
+                    //load data from data.json
+
+                    //TODO: obtain all records in one day
+                    JSONArray jsonArray = new JSONArray();
+
+                    AtomicInteger counter = new AtomicInteger();
+                    AtomicInteger value = new AtomicInteger();
+                    habitProgressList.get(position).getProgressList().forEach(progress -> {
+                        value.getAndIncrement();
+                        JSONObject jsonObject = new JSONObject();
+                        try {
+
+                            counter.addAndGet(progress.getCounter());
+                            jsonObject.put("progress", counter.get());
+
+                            new Date(progress.getUpdatedDate());
+                            jsonObject.put("date", value.get());
+
+                            jsonArray.put(jsonObject);
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                        System.out.println(progress.getCounter() + "-" + new Date(progress.getUpdatedDate()));
+                    });
+
+
+                    //set data to chart
+
+                    String source = jsonArray.toString();
+                    System.out.println(source);
+                    mChart.source(source);
+
+                    mChart.setScale("progress", new F2Chart.ScaleConfigBuilder().precision(0).max(Double.parseDouble(habitProgressList.get(position).getHabit().getFrequency())).min(0));
+                    mChart.setScale("date", new F2Chart.ScaleConfigBuilder().precision(1).tickCount(7).range(new double[]{0.1, 0.9}));
+
+
+                    //Draw a polyline on the chart. The data mappings of the x-axis and y-axis of the polyline are genre and sold fields respectively.
+                    mChart.line().color("type", new String[]{"ORANGE"})
+                            .fixedSize(2.f)
+                            .fixedShape("smooth")
+                            .position("date*progress");
+
+                    //Render and display on canvasView
+                    mChart.render();
+                }
+
+                @Override
+                public void onDestroy() {
+                    if (mChart != null) {
+                        mChart.destroy();
+                    }
+                }
+            });
+
         });
 
-        recycleView.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false));
+        recycleView.setLayoutManager(new GridLayoutManager(requireContext(), 2, LinearLayoutManager.VERTICAL, false));
         recycleView.setAdapter(todayReportAdapter);
 
     }
@@ -101,125 +139,28 @@ public class TodayReportFragment extends Fragment implements F2CanvasView.Adapte
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+
+        habitViewModel.getAllProgress().observe(requireActivity(), habitProgresses -> {
+            habitProgressList.clear();
+            habitProgresses.forEach(habitProgress -> {
+                habitProgressList.add(habitProgress);
+            });
+
+            todayReportAdapter.notifyDataSetChanged();
+        });
+
         return binding.getRoot();
     }
+
 
     private static void onGraphChange(HorizontalBarChart barChart, int position) {
 
     }
 
-    private LineData setData(int count, float range) {
-        LineData data = null;
-        ArrayList<Entry> values = new ArrayList<>();
-
-        for (int i = 0; i < count; i++) {
-
-            float val = (float) (Math.random() * range) - 30;
-            values.add(new Entry(i, val));
-        }
-
-        LineDataSet set1;
-
-        if (chart.getData() != null &&
-                chart.getData().getDataSetCount() > 0) {
-            set1 = (LineDataSet) chart.getData().getDataSetByIndex(0);
-            set1.setValues(values);
-            set1.notifyDataSetChanged();
-            chart.getData().notifyDataChanged();
-            chart.notifyDataSetChanged();
-        } else {
-            // create a dataset and give it a type
-            set1 = new LineDataSet(values, "");
-
-            set1.setDrawIcons(false);
-            //Remove circle from the line chart
-            set1.setDrawCircles(false);
-            set1.setHighlightEnabled(false);
-
-            // draw dashed line
-            //set1.enableDashedLine(10f, 5f, 0f);
-
-            // black lines and points
-            set1.setColor(getResources().getColor(R.color.yellow));
-            set1.setCircleColor(getResources().getColor(R.color.yellow));
-
-            // line thickness and point size
-            set1.setLineWidth(2f);
-            //set1.setCircleRadius(3f);
-
-            // draw points as solid circles
-            set1.setDrawCircleHole(false);
-
-            // customize legend entry
-            /*set1.setFormLineWidth(1f);
-            set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
-            set1.setFormSize(15.f);
-*/
-
-            set1.setDrawHighlightIndicators(false);
-            set1.setDrawVerticalHighlightIndicator(false);
-            set1.setDrawHorizontalHighlightIndicator(false);
-
-            //Remove value from the chart
-            set1.setDrawValues(false);
-
-            // text size of values
-            set1.setValueTextSize(9f);
-
-            // draw selection line as dashed
-            //set1.enableDashedHighlightLine(10f, 5f, 0f);
-
-            // set the filled area
-            set1.setDrawFilled(false);
-            set1.setFillFormatter(new IFillFormatter() {
-                @Override
-                public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
-                    return chart.getAxisLeft().getAxisMinimum();
-                }
-            });
-
-            // set color of filled area
-
-            set1.setFillColor(Color.BLACK);
-
-            ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-            dataSets.add(set1); // add the data sets
-
-            // create a data object with the data sets
-            data = new LineData(dataSets);
-
-            // set data
-
-        }
-        return data;
-    }
-
     @Override
     public void onCanvasDraw(F2CanvasView f2CanvasView) {
-        //Initialize the chart, because you need the width and height of the canvasView,
-        //initialize here to ensure that you can get the width and height of the canvasView
-        if (mChart == null) {
 
-            mChart = F2Chart.create(
-                    canvasView.getContext(),
-                    "LineChart-Kotlin",
-                    canvasView.getWidth(),
-                    canvasView.getHeight()
-            );
-        }
-        //Associating the chart and canvasView, the chart finally needs to display the chart on the canvasView
-        mChart.setCanvas(canvasView);
-        //set chart padding
-        mChart.padding(10.0, 10.0, 10.0, 10.0);
-        //load data from data.json
-        String source = Utils.loadAssetFile(canvasView.getContext(), "data.json");
-
-        //set data to chart
-        mChart.source(source);
-        //Draw a polyline on the chart. The data mappings of the x-axis and y-axis of the polyline are genre and sold fields respectively.
-        mChart.line().color("type") .fixedSize(1.f) .fixedShape("smooth").position("genre*sold");
-
-        //Render and display on canvasView
-        mChart.render();
     }
+
+
 }
