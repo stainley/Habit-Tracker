267fad14bfdccbe8f26dac3a625bfcfa441278ca
Ann Robles <annr.dev@gmail.com>
2023-03-24 21:07:05 -0400

    HAB-144 Created AchievementGridAdapter

---
+++ app/src/main/java/ca/lambton/habittracker/view/ongoingHabits/AchievementGridAdapter.java
@@ -0,0 +1,45 @@
+package ca.lambton.habittracker.view.ongoingHabits;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.card.MaterialCardView;
+
+import java.util.ArrayList;
+
+import ca.lambton.habittracker.R;
+
+public class AchievementGridAdapter extends ArrayAdapter<AchievementInfo> {
+
+    public AchievementGridAdapter(@NonNull Context context, ArrayList<AchievementInfo> achievementModelArrayList) {
+        super(context, 0, achievementModelArrayList);
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View listitemView = convertView;
+        if (listitemView == null) {
+            // Layout Inflater inflates each item to be displayed in GridView.
+            listitemView = LayoutInflater.from(getContext()).inflate(R.layout.grid_achievement, parent, false);
+        }
+
+        AchievementInfo achievementInfo = getItem(position);
+        TextView achievementLabel = listitemView.findViewById(R.id.achievementLabel);
+        MaterialCardView achievementsCard = listitemView.findViewById(R.id.achievementsCard);
+        //ImageView achievementImage = listitemView.findViewById(R.id.achievementImage);
+        achievementLabel.setText(achievementInfo.getAchievementLabel());
+
+        return listitemView;
+    }
+}
