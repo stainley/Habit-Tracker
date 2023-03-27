c136fa8c4e2a86e0cececb5fa89310655a448b8b
Stainley Lebron <saiyamans@gmail.com>
2023-03-27 18:15:50 -0400

    HAB-28 initial login functionality

--- app/src/main/java/ca/lambton/habittracker/view/LoginFragment.java
+++ app/src/main/java/ca/lambton/habittracker/view/LoginFragment.java
@@ -1,47 +1,211 @@
 package ca.lambton.habittracker.view;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.squareup.picasso.Picasso;
+
+import ca.lambton.habittracker.MainActivity;
 import ca.lambton.habittracker.R;
+import ca.lambton.habittracker.databinding.FragmentLoginBinding;
 
-public class LoginFragment extends Fragment {
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class LoginFragment extends AppCompatActivity {
+    FragmentLoginBinding binding;
 
-    private String mParam1;
-    private String mParam2;
+    private FirebaseAuth mAuth;
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signInRequest;
+    private static final int REQ_ONE_TAP = 2;  // Can be any integer unique to the Activity.
+    private boolean showOneTapUI = true;
 
-    public LoginFragment() {
-        // Required empty public constructor
-    }
-
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        binding = FragmentLoginBinding.inflate(LayoutInflater.from(getApplicationContext()));
+        setContentView(binding.getRoot());
+
+        binding.loginGoogle.setOnClickListener(this::loginWithGoogle);
+
+        binding.loginBtn.setOnClickListener(this::loginWithEmail);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        oneTapClient = Identity.getSignInClient(this);
+
+
+        signInRequest = BeginSignInRequest.builder().setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder().setSupported(true).build()).setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder().setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.default_web_client_id))
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(false).build())
+                // Automatically sign in when exactly one credential is retrieved.
+                .setAutoSelectEnabled(true).build();
+    }
+
+    private void loginWithEmail(View view) {
+        String email = binding.emailLoginText.getText().toString();
+        String password = binding.passwordLoginText.getText().toString();
+
+        signInEmailAndPassword(email, password);
+    }
+
+
+    /**
+     *
+     */
+    private void signInEmailAndPassword(String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                // Sign in successful
+                Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainIntent);
+            } else {
+                // sign in failed
+                Toast.makeText(getApplicationContext(), "User/Password incorrect", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loginWithGoogle(View view) {
+        Toast.makeText(getApplicationContext(), "CLICKCED", Toast.LENGTH_SHORT).show();
+
+        oneTapClient.beginSignIn(signInRequest).addOnSuccessListener(LoginFragment.this, result -> {
+            try {
+                startIntentSenderForResult(result.getPendingIntent().getIntentSender(), REQ_ONE_TAP, null, 0, 0, 0);
+            } catch (IntentSender.SendIntentException e) {
+                Log.e("TAG", "Couldn't start One Tap UI: " + e.getLocalizedMessage());
+            }
+        }).addOnFailureListener(LoginFragment.this, e -> {
+            // No saved credentials found. Launch the One Tap sign-up flow, or
+            // do nothing and continue presenting the signed-out UI.
+            Log.d("TAG", e.getLocalizedMessage());
+        });
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
+
+        return super.onCreateView(name, context, attrs);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_ONE_TAP:
+                try {
+                    SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(data);
+                    String idToken = credential.getGoogleIdToken();
+                    String username = credential.getId();
+                    String password = credential.getPassword();
+                    if (idToken != null) {
+                        // Got an ID token from Google. Use it to authenticate
+                        // with your backend.
+                        Log.d("TAG", "Got ID token.");
+
+                        credential.getProfilePictureUri();
+
+                        AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
+                        mAuth.signInWithCredential(firebaseCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("TAG", "signInWithCredential:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    updateUI(user);
+
+                                    Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(mainIntent);
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("TAG", "signInWithCredential:failure", task.getException());
+                                    updateUI(null);
+                                }
+                            }
+                        });
+                    } else if (password != null) {
+                        // Got a saved username and password. Use them to authenticate
+                        // with your backend.
+                        Log.d("TAG", "Got password.");
+                    }
+                } catch (ApiException e) {
+                    switch (e.getStatusCode()) {
+                        case CommonStatusCodes.CANCELED:
+                            Log.d("TAG", "One-tap dialog was closed.");
+                            // Don't re-prompt the user.
+                            showOneTapUI = false;
+                            break;
+                        case CommonStatusCodes.NETWORK_ERROR:
+                            Log.d("TAG", "One-tap encountered a network error.");
+                            // Try again or just ignore.
+                            break;
+                        default:
+                            Log.d("TAG", "Couldn't get credential from result." + e.getLocalizedMessage());
+                            break;
+                    }
+                    Toast.makeText(this, "Login aborted", Toast.LENGTH_SHORT).show();
+                }
+                break;
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        if (currentUser != null) {
+            Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(mainIntent);
+        }
+    }
+
+    private void signOut() {
+        FirebaseAuth.getInstance().signOut();
     }
 }
