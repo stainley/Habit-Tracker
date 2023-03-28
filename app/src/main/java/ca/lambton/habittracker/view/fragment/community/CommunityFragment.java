dd23dd92a7b6cea1568f9b6e7463f9dbb93b41dd
Stainley Lebron <saiyamans@gmail.com>
2023-03-28 10:11:56 -0400

    Show community screen

--- app/src/main/java/ca/lambton/habittracker/view/fragment/community/CommunityFragment.java
+++ app/src/main/java/ca/lambton/habittracker/view/fragment/community/CommunityFragment.java
@@ -1,6 +1,44 @@
 package ca.lambton.habittracker.view.fragment.community;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+
+import ca.lambton.habittracker.databinding.FragmentCommunityBinding;
 
 public class CommunityFragment extends Fragment {
+
+    private FragmentCommunityBinding binding;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = FragmentCommunityBinding.inflate(LayoutInflater.from(requireContext()));
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.btnCompose.setOnClickListener(this::newPost);
+    }
+
+    private void newPost(View view) {
+        NavDirections navNewPostDirections = CommunityFragmentDirections.actionNavCommunityToNavCompose();
+        Navigation.findNavController(requireView()).navigate(navNewPostDirections);
+    }
 }
